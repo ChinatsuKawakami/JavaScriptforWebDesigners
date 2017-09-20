473b218ad7ea3bf41a9e78df1566b606ffecc6ce
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-20 13:20:39 -0400

    0.0.3 add function to check submit event works or not

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_05/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_05/script.js
@@ -1,6 +1,18 @@
+
+// Edit Date: 2017-09-20
+// Editor : Chinatsu Kawakami
+//version 0.0.3 add function to check submit event works or not
+
 (function() {
 "use strict";
 
+document.getElementById('cart-hplus').addEventListener('submit',estimateTotal);
+
+function estimateTotal(event){
+	event.preventDefault();
+    
+    console.log('you submitted');
+}
 
 
 })();
