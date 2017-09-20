59a51dea560a553fdb0604e369f78e495ce1c3aa
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-20 13:37:48 -0400

    0.0.4 add function to show alert when the user forget to select status of shopping

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_06/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_06/script.js
@@ -1,3 +1,7 @@
+
+// Edit Date: 2017-09-20
+// Editor : Chinatsu Kawakami
+//version 0.0.4 add function to show alert when the user forget to select status of shopping
 (function() {
 "use strict";
 
@@ -5,8 +9,15 @@ document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
 
 function estimateTotal(event) {
 	event.preventDefault();
+
+	var state = document.getElementById('s-state');
 	
-	console.log('You submitted the form.');
+	if(state.value === ''){
+        alert('please choose your shopping status.');
+        state.focus();
+	}
+
+	//console.log('You submitted the form.');
 }
 
 })();
