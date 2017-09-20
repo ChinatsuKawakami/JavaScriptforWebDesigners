512294ba86a36f4352796ba31df126fac9309d8f
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-20 14:59:22 -0400

    0.0.5 add function to make the submit button disable.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_07/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_07/script.js
@@ -1,12 +1,37 @@
+
+// Edit Date: 2017-09-20
+// Editor : Chinatsu Kawakami
+//version 0.0.5 add function to make the submit button disable.
+
 (function() {
 "use strict";
+	var state = document.getElementById('s-state');
+
+ document.addEventListener('DOMContentLoaded', function(){
+	document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+     
+
+	var btnEstimate = document.getElementById('btn-estimate');
+ 
+    btnEstimate.disabled = true;
+
+  state.addEventListener('change',function(){
+     	if(state.value === ''){
+       btnEstimate.disabled = true;
+	}else{
+		 btnEstimate.disabled = false;
+	}
+
+
+     });
+	
+});
 
 document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
 
 function estimateTotal(event) {
 	event.preventDefault();
 	
-	var state = document.getElementById('s-state');
 	
 	if (state.value === '') {
 		alert('Please choose your shipping state.');
