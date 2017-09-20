dfa9b1cc7ce4d80f4f85dbef6dfe70edf8f573bb
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-20 15:37:57 -0400

    0.0.7 add the function to show total cost

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_09/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch03/03_09/script.js
@@ -1,3 +1,7 @@
+// Edit Date: 2017-09-20
+// Editor : Chinatsu Kawakami
+//version 0.0.7 add the function to show total cost
+
 (function() {
 "use strict";
 
@@ -36,7 +40,44 @@ function estimateTotal(event) {
 		shippingState = state.value,
 		shippingMethod = document.querySelector('[name=r_method]:checked').value;
 		
-	console.log(itemBball, itemJersey, itemPower, shippingState, shippingMethod);
+	//console.log(itemBball, itemJersey, itemPower, shippingState, shippingMethod);
+
+	var totalQty = itemPower+itemBball+itemJersey,
+	    shippingCostper,
+	    shippingCost,
+	    taxFactor=1,
+	    estimate,
+	    totalItemprice = (90 * itemBball) + (25 * itemJersey)+(30 * itemPower);
+	    
+        
+        if(shippingState==='CA'){
+        	taxFactor = 1.075;
+        }
+
+        switch(shippingMethod){
+        	case 'usps':
+        	  shippingCostper = 2;
+        	  break;
+        	case 'ups':
+        	  shippingCostper = 3;
+        	  break;
+        	default:
+        	  shippingCostper = 0;
+        	  break;
+
+        }
+
+        shippingCost = shippingCostper * totalQty;
+
+	  
+       /* if(shippingState==='pickup'){
+        	shippingCostper = 0;
+        }else if(shippingMethod==='usps'){
+            shippingCostper = 2;
+        }*/
+	    estimate = (totalItemprice * taxFactor) + shippingCost;
+
+	    document.getElementById('txt-estimate').value = estimate;
 }
 
 })();
