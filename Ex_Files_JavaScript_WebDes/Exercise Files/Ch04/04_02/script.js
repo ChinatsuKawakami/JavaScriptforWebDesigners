ab4c60e727911e9c3dec309ca8b0161d4d77ca06
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-21 18:21:18 -0400

    version 0.1.4 add the function to show the current time only has hours and minutes.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_02/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_02/script.js
@@ -1,3 +1,7 @@
+// Edit Date: 2017-09-21
+// Editor : Chinatsu Kawakami
+//version 0.1.4 add the function to show the current time only has hours and minutes.
+
 (function(){
 'use strict';
 
@@ -5,9 +9,12 @@ document.addEventListener('DOMContentLoaded', function(){
 	var c = document.getElementById('current-time');
 	
 	var d = new Date();
+	var hours = d.getHours();
+	if(hours>12) {
+		hours -=12;
+	}
 	
-	c.innerHTML = d.toTimeString();
+	c.innerHTML = hours + ':' + d.getMinutes();
 });
-
 
 })();
