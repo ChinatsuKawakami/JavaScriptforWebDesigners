3b3abf90963eb95e7fffbc555fb09202bd1aabce
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-21 18:36:46 -0400

    version 0.1.5 edited function to show the : in the time.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_03/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_03/script.js
@@ -1,17 +1,27 @@
+// Edit Date: 2017-09-21
+// Editor : Chinatsu Kawakami
+//version 0.1.5 edited function to show the : in the time.
+
 (function(){
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function(){
 	var c = document.getElementById('current-time');
 	
-	var d = new Date();
-	
+
+    setInterval(updateTime,1000);
+
+    function updateTime(){
+    var d = new Date();
 	var hours = d.getHours();
 	if (hours > 12) {
 		hours -= 12;
 	}
 	
-	c.innerHTML = hours + ':' + d.getMinutes();
+	var sep = ':';
+	if(d.getSeconds()%2 == 1) sep = ' ';
+	c.innerHTML = hours + sep + d.getMinutes();
+}
 });
 
 
