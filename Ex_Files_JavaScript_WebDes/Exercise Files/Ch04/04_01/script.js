fdfb2617e657cfe670358c5f6e9743b393953277
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-21 18:10:12 -0400

    0.1.3 add the function to show the current time.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_01/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_01/script.js
@@ -1,6 +1,18 @@
+// Edit Date: 2017-09-21
+// Editor : Chinatsu Kawakami
+//version 0.1.3 add the function to show the current time.
+
 (function(){
 'use strict';
 
+document.addEventListener('DOMContentLoaded',function(){
+
+var c = document.getElementById('current-time');
+var d = new Date();
+
+c.innerHTML = d.toTimeString();
+
+});
 
 
 })();
