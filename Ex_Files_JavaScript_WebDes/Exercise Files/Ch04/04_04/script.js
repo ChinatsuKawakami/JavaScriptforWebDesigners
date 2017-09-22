ec3782afa6b7f07f078158a2bde3269d91fb7024
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-22 10:39:26 -0400

    version 0.1.6 polish the clock.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_04/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch04/04_04/script.js
@@ -1,23 +1,41 @@
+// Edit Date: 2017-09-22
+// Editor : Chinatsu Kawakami
+//version 0.1.6 polish the clock.
+
 (function(){
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function(){
 	var c = document.getElementById('current-time');
 	
+	//const time = 'AM';
 	setInterval(updateTime, 1000);
 	
 	function updateTime() {
 		var d = new Date();
 	
-		var hours = d.getHours();
+		var hours = d.getHours(),
+		    minutes = d.getMinutes(),
+		    ampm=' AM';
 		if (hours > 12) {
 			hours -= 12;
+            ampm = ' PM'
+            //document.insertHTMLBefore('current-time') = sep;
+		}else if(hours===0){
+			hours = 12;
 		}
+
+		if(minutes<10){
+			minutes='0'+minutes;
+		}
+
 		
-		var sep = ':';
-		if (d.getSeconds() % 2 === 1) sep = ' ';
+		var sepClass = ':';
+		if (d.getSeconds() % 2 === 1) sepClass = 'trans';
+
+		var sep = '<span class="'+sepClass+'">:</span>'
 	
-		c.innerHTML = hours + sep + d.getMinutes();
+		c.innerHTML = hours + sep + minutes+' '+ampm;
 	}
 });
 
