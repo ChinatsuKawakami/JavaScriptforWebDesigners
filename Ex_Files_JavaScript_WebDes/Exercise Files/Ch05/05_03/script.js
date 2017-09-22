19cbf65e0ca007fc1a7548393dd452417e93751d
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-22 16:00:09 -0400

    0.2.0 Change the center point.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_03/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_03/script.js
@@ -1,9 +1,16 @@
-var gMapAPIKey = 'YOUR_API_KEY_HERE';
+/*
+// Edit Date: 2017-09-22
+// Editor : Chinatsu Kawakami
+//version 0.2.0 Change the center point.
+
+*/
+
+var gMapAPIKey = 'AIzaSyAvIWyZ3JXDqozzp8aZOLwcfR0brTSIWkI';
 
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('hplus-map'), {
-		center: {lat: -34.397, lng: 150.644},
+		center: {lat: 34.1031252, lng: -118.3263873},
 		zoom: 8
 	});
 }
