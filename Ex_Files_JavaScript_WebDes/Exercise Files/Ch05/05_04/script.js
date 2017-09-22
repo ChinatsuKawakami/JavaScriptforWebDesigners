001e1e0ba4378ec745f93d98d7d4dbda3890f03d
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-22 16:38:32 -0400

    version 0.2.1 Change the type and zoom level.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_04/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_04/script.js
@@ -1,9 +1,18 @@
-var gMapAPIKey = 'YOUR_API_KEY_HERE';
+/*
+// Edit Date: 2017-09-22
+// Editor : Chinatsu Kawakami
+//version 0.2.1 Change the type and zoom level.
+*/
+
+var gMapAPIKey = 'AIzaSyDC_wGSSyUGw3KPps1f5pceJwr8TC6HWzg';
 
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('hplus-map'), {
-		center: {lat: 34.1031131, lng: -118.326343},
-		zoom: 8
+		'center': {lat: 34.1031131, lng: -118.326343},
+		'zoom': 8,
+		'mapTypeId': google.maps.MapTypeId.ROADMAP,
+		'draggable':false,
+		'scrollwheel':false
 	});
 }
