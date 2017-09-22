f9f97d8225bc81dee82ac0d221102f89b22976ce
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-09-22 16:50:06 -0400

    version 0.2.2 Add a marker.

--- Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_05/script.js
+++ Ex_Files_JavaScript_WebDes/Exercise Files/Ch05/05_05/script.js
@@ -1,10 +1,23 @@
+/*
+// Edit Date: 2017-09-22
+// Editor : Chinatsu Kawakami
+//version 0.2.2 Add a marker.
+*/
+var gMapAPIKey = 'AIzaSyDC_wGSSyUGw3KPps1f5pceJwr8TC6HWzg';
 var map;
 function initMap() {
+	var storeLocation = {lat: 34.1031131, lng: -118.326343};
 	map = new google.maps.Map(document.getElementById('hplus-map'), {
-		'center' : {lat: 34.1031131, lng: -118.326343},
+		'center' : storeLocation,
 		'zoom' : 12,
 		'mapTypeId' : google.maps.MapTypeId.ROADMAP,
 		'draggable' : false,
 		'scrollwheel' : false
 	});
+
+	  var marker = new google.maps.Marker({
+          'position': storeLocation,
+          'map': map,
+          'title': 'hplus sport in Los Angeles'
+        });
 }
